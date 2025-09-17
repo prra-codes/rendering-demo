86a4d566254029ec294865ff356009a407b236f2
peter <peter.arth5@gmail.com>
2025-09-17 08:59:43 +0100

    done a bit more Next.js learning

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,8 +1,33 @@
 'use client';
 
-import { serverSideFunction } from '@/utils/server-utils';
+// import { serverSideFunction } from '@/utils/server-utils';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function ClientRoutePage() {
-  const result = serverSideFunction();
-  return <h1>Client Route {result}</h1>;
+  // const result = serverSideFunction();
+  // return <h1>Client Route {result}</h1>;
+  const settings = {
+    dots: true,
+  };
+  return (
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
